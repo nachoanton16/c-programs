6c0cd5adc2d1b929277cf10a263bb9751db84cc8
nachoanton16 <nacho1a3@gmail.com>
2018-03-02 14:32:52 +0100

    proyecto

---
+++ 13_lista/frecuencias.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    char mirar;
+    char contadora;//*contador
+    char contadorb;//*contador+1
+    char contadorc;//*contador+2
+    char contadord;//*contador+3
+    char contadore;//*contador+4
+    char contadorf;//*contador+5
+    char contadorg;//*contador+6
+    char contadorh;//*contador+7
+    char contadori;//*contador+8
+    char contadorj;//*contador+9
+    char contadork;//*contador+10
+    char contadorl;//*contador+11
+    char contadorm;//*contador+12
+    char contadorn;//*contador+13
+    char contadorñ;//*contador+14
+    char contadoro;//*contador+15
+    char contadorp;//*contador+16
+    char contadorq;//*contador+17
+    char contadorr;//*contador+18
+    char contadors;//*contador+19
+    char contadort;//*contador+20
+    char contadoru;//*contador+21
+    char contadorv;//*contador+22
+    char contadorw;//*contador+23
+    char contadorx;//*contador+24
+    char contadory;//*contador+25
+    char contadorz;//*contador+26
+
+
+    printf("Escribe un texto: ");
+
+    mirar = getc(stdin);
+    if (mirar == '\n'){
+        ungetc(mirar, stdin);
+        scanf(" %c", &contadora);
+    }
+
+    if(mirar == '\n')
+        ultimo = true;
+
+}
+
+scanf(" %*1[][,.()]", );
+
+return EXIT_SUCCESS;
+}
