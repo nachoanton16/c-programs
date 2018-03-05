d4556c05b0f6a9c6ceca5de2427e2a1f8a7220f3
Antón <nacho1a3@gmail.com>
2018-03-05 22:50:56 +0100

    proyecto actualizado

--- 13_lista/frecuencias.cpp
+++ 13_lista/frecuencias.cpp
@@ -3,50 +3,202 @@
 
 int main(){
 
-    char mirar;
-    char contadora;//*contador
-    char contadorb;//*contador+1
-    char contadorc;//*contador+2
-    char contadord;//*contador+3
-    char contadore;//*contador+4
-    char contadorf;//*contador+5
-    char contadorg;//*contador+6
-    char contadorh;//*contador+7
-    char contadori;//*contador+8
-    char contadorj;//*contador+9
-    char contadork;//*contador+10
-    char contadorl;//*contador+11
-    char contadorm;//*contador+12
-    char contadorn;//*contador+13
-    char contadorñ;//*contador+14
-    char contadoro;//*contador+15
-    char contadorp;//*contador+16
-    char contadorq;//*contador+17
-    char contadorr;//*contador+18
-    char contadors;//*contador+19
-    char contadort;//*contador+20
-    char contadoru;//*contador+21
-    char contadorv;//*contador+22
-    char contadorw;//*contador+23
-    char contadorx;//*contador+24
-    char contadory;//*contador+25
-    char contadorz;//*contador+26
+    int mirar;
+    int contadora;//*contador
+    int contadorb;//*contador+1
+    int contadorc;//*contador+2
+    int contadord;//*contador+3
+    int contadore;//*contador+4
+    int contadorf;//*contador+5
+    int contadorg;//*contador+6
+    int contadorh;//*contador+7
+    int contadori;//*contador+8
+    int contadorj;//*contador+9
+    int contadork;//*contador+10
+    int contadorl;//*contador+11
+    int contadorm;//*contador+12
+    int contadorn;//*contador+13
+    int contadorñ;//*contador+14
+    int contadoro;//*contador+15
+    int contadorp;//*contador+16
+    int contadorq;//*contador+17
+    int contadorr;//*contador+18
+    int contadors;//*contador+19
+    int contadort;//*contador+20
+    int contadoru;//*contador+21
+    int contadorv;//*contador+22
+    int contadorw;//*contador+23
+    int contadorx;//*contador+24
+    int contadory;//*contador+25
+    int contadorz;//*contador+26
 
 
     printf("Escribe un texto: ");
 
     mirar = getc(stdin);
-    if (mirar == '\n'){
+    if (mirar == 'a' || mirar == 'A'){
         ungetc(mirar, stdin);
-        scanf(" %c", &contadora);
+        scanf(" %i", &contadora);
     }
 
-    if(mirar == '\n')
-        ultimo = true;
+    if (mirar == 'b' || mirar == 'B'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorb);
+    }
 
-}
+    if (mirar == 'c' || mirar == 'C'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorc);
+    }
 
-scanf(" %*1[][,.()]", );
+    if (mirar == 'd' || mirar == 'D'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadord);
+    }
 
-return EXIT_SUCCESS;
+    if (mirar == 'e' || mirar == 'E'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadore);
+    }
+
+    if (mirar == 'f' || mirar == 'F'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorf);
+    }
+
+    if (mirar == 'g' || mirar == 'G'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorg);
+    }
+
+    if (mirar == 'h' || mirar == 'H'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorh);
+    }
+
+    if (mirar == 'i' || mirar == 'I'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadori);
+    }
+
+    if (mirar == 'j' || mirar == 'J'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorj);
+    }
+
+    if (mirar == 'k' || mirar == 'K'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadork);
+    }
+
+    if (mirar == 'l' || mirar == 'L'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorl);
+    }
+
+    if (mirar == 'm' || mirar == 'M'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorm);
+    }
+
+    if (mirar == 'n' || mirar == 'N'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorn);
+    }
+
+    if (mirar == 'ñ' || mirar == 'Ñ'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorñ);
+    }
+
+    if (mirar == 'o' || mirar == 'O'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadoro);
+    }
+
+    if (mirar == 'p' || mirar == 'P'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorp);
+    }
+
+    if (mirar == 'q' || mirar == 'Q'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorq);
+    }
+
+    if (mirar == 'r' || mirar == 'R'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorr);
+    }
+
+    if (mirar == 's' || mirar == 'S'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadors);
+    }
+
+    if (mirar == 't' || mirar == 'T'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadort);
+    }
+
+    if (mirar == 'u' || mirar == 'U'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadoru);
+    }
+
+    if (mirar == 'v' || mirar == 'V'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorv);
+    }
+
+    if (mirar == 'w' || mirar == 'W'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorw);
+    }
+
+    if (mirar == 'x' || mirar == 'X'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorx);
+    }
+
+    if (mirar == 'y' || mirar == 'Y'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadory);
+    }
+
+    if (mirar == 'z' || mirar == 'Z'){
+        ungetc(mirar, stdin);
+        scanf(" %i", &contadorz);
+    }
+
+    printf("Hay %i en el texto.\n", contadora);
+    printf("Hay %i en el texto.\n", contadorb);
+    printf("Hay %i en el texto.\n", contadorc);
+    printf("Hay %i en el texto.\n", contadord);
+    printf("Hay %i en el texto.\n", contadore);
+    printf("Hay %i en el texto.\n", contadorf);
+    printf("Hay %i en el texto.\n", contadorg);
+    printf("Hay %i en el texto.\n", contadorh);
+    printf("Hay %i en el texto.\n", contadori);
+    printf("Hay %i en el texto.\n", contadorj);
+    printf("Hay %i en el texto.\n", contadork);
+    printf("Hay %i en el texto.\n", contadorl);
+    printf("Hay %i en el texto.\n", contadorm);
+    printf("Hay %i en el texto.\n", contadorn);
+    printf("Hay %i en el texto.\n", contadorñ);
+    printf("Hay %i en el texto.\n", contadoro);
+    printf("Hay %i en el texto.\n", contadorp);
+    printf("Hay %i en el texto.\n", contadorq);
+    printf("Hay %i en el texto.\n", contadorr);
+    printf("Hay %i en el texto.\n", contadors);
+    printf("Hay %i en el texto.\n", contadort);
+    printf("Hay %i en el texto.\n", contadoru);
+    printf("Hay %i en el texto.\n", contadorv);
+    printf("Hay %i en el texto.\n", contadorw);
+    printf("Hay %i en el texto.\n", contadorx);
+    printf("Hay %i en el texto.\n", contadory);
+    printf("Hay %i en el texto.\n", contadorz);
+
+
+    return EXIT_SUCCESS;
 }
